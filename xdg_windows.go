1e56e35b13f4ee71de8450c64ae9103b592718ae
Will Dixon <dixonwille@hotmail.com>
2017-07-20 13:26:50 -0400

    Added copyright to all the files

--- xdg_windows.go
+++ xdg_windows.go
@@ -1,3 +1,7 @@
+// Copyright (c) 2017, OpenPeeDeeP. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
 package xdg
 
 import "os"
